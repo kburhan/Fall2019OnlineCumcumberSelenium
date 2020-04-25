36abc7fae34267e65675edfc71fadd480743665c
kburhan <kburhan07@gmail.com>
2020-04-25 13:53:50 -0400

    first commit

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,23 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources",
+        dryRun = false,
+        strict = false,
+//        tags = "@driver",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json"
+        }
+
+)
+public class CucumberRunner {
+
+
+}
